66750552afcfe0f45a8340350f13f11b40c09720
MaxOliveira <maxwell_oliveira2@hotmail.com>
2014-08-26 19:34:42 -0300

    Padronizando comentarios

--- src/main/java/br/com/mdsgpp/guiaescolaideal/control/ContatoControl.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/control/ContatoControl.java
@@ -25,7 +25,7 @@ public class ContatoControl {
      * e a porta usada por ele 
      */  
     
-  // Para o GMAIL
+  // Contato para o GMAIL
     public ContatoControl() {    
         mailSMTPServer = "smtp.gmail.com";  
         mailSMTPServerPort = "465";  
@@ -39,17 +39,17 @@ public class ContatoControl {
      */  
     
   // Para outro Servidor
-    public ContatoControl(String mailSMTPServer, String mailSMTPServerPort) {   
+    public ContatoControl(String mailSMTPServer, String mailSMTPServerPort) { 
         this.mailSMTPServer = mailSMTPServer;  
         this.mailSMTPServerPort = mailSMTPServerPort; 
-        
-        this.session = configuraSession();
+        this.session = configuraSession();  
     }  
      
-    public ContatoControl(Session session){
+    public ContatoControl(Session session) {
 	this.session = session;
     }
     
+    // Usuario ou seja, a conta que esta enviando o email (tem que ser do GMAIL)
     public void sendMail(String from, String to, String subject, String message) throws ContatoException {  
         session.getProperties().put("mail.smtp.user", from); // usuario ou seja, a conta que esta enviando o email (tem que ser do GMAIL)
         
@@ -57,13 +57,13 @@ public class ContatoControl {
         Message msg = new MimeMessage(session);  
   
         try {  
-            //Setando o destinatário  
+            // Setando o destinatário  
             msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));  
-            //Setando a origem do email  
+            // Setando a origem do email  
             msg.setFrom(new InternetAddress(from));  
-            //Setando o assunto  
+            // Setando o assunto  
             msg.setSubject(subject);  
-            //Setando o conteúdo/corpo do email  
+            // Setando o conteúdo/corpo do email  
             msg.setContent(message,"text/plain");  
   
         } catch (Exception e) {  
@@ -72,18 +72,22 @@ public class ContatoControl {
              
         }  
           
-        //Objeto encarregado de enviar os dados para o email  
+        // Objeto encarregado de enviar os dados para o email  
         Transport tr;  
         try {  
-            tr = session.getTransport("smtp"); //define smtp para transporte  
-            /* 
+            // Define smtp para transporte 
+            tr = session.getTransport("smtp"); 
+            
+            /** 
              *  1 - define o servidor smtp 
              *  2 - seu nome de usuario do gmail 
              *  3 - sua senha do gmail 
              */  
+            
             tr.connect(mailSMTPServer, "contato.guiaescolaideal@gmail.com", "4t6Li40vZt");  
-            msg.saveChanges(); // don't forget this  
-            //envio da mensagem  
+            msg.saveChanges(); // Don't forget this
+            
+            // Envio da mensagem  
             tr.sendMessage(msg, msg.getAllRecipients());  
             tr.close();  
         } catch (Exception e) {  
@@ -95,38 +99,41 @@ public class ContatoControl {
     private Session configuraSession() {
 	Properties props = new Properties();  
         
-                // quem estiver utilizando um SERVIDOR PROXY descomente essa parte e atribua as propriedades do SERVIDOR PROXY utilizado  
-                /* 
+                // Quem estiver utilizando um SERVIDOR PROXY descomente essa parte e atribua as propriedades do SERVIDOR PROXY utilizado  
+                /** 
                 props.setProperty("proxySet","true"); 
                 props.setProperty("socksProxyHost","192.168.155.1"); // IP do Servidor Proxy 
                 props.setProperty("socksProxyPort","1080");  // Porta do servidor Proxy 
                 */  
   
-        props.put("mail.transport.protocol", "smtp"); //define protocolo de envio como SMTP  
+        props.put("mail.transport.protocol", "smtp"); // Define protocolo de envio como SMTP  
         props.put("mail.smtp.starttls.enable","true");   
-        props.put("mail.smtp.host", mailSMTPServer); //server SMTP do GMAIL  
-        props.put("mail.smtp.auth", "true"); //ativa autenticacao  
+        props.put("mail.smtp.host", mailSMTPServer); // Server SMTP do GMAIL  
+        props.put("mail.smtp.auth", "true"); // Ativa autenticacao  
         props.put("mail.debug", "true");  
-        props.put("mail.smtp.port", mailSMTPServerPort); //porta  
-        props.put("mail.smtp.socketFactory.port", mailSMTPServerPort); //mesma porta para o socket  
+        props.put("mail.smtp.port", mailSMTPServerPort); // Porta  
+        props.put("mail.smtp.socketFactory.port", mailSMTPServerPort); // Mesma porta para o socket  
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");  
         props.put("mail.smtp.socketFactory.fallback", "false");  
           
-        //Cria um autenticador que sera usado a seguir  
+        // Cria um autenticador que sera usado a seguir  
         SimpleAuth auth = null;  
         auth = new SimpleAuth ("contato.guiaescolaideal@gmail.com","4t6Li40vZt");  
           
         //Session - objeto que ira realizar a conexão com o servidor  
-        /*Como há necessidade de autenticação é criada uma autenticacao que 
+        
+        /**
+         *Como há necessidade de autenticação é criada uma autenticacao que 
          * é responsavel por solicitar e retornar o usuário e senha para  
          * autenticação */  
+        
         Session session = Session.getDefaultInstance(props, auth);  
-        session.setDebug(true); //Habilita o LOG das ações executadas durante o envio do email  
+        session.setDebug(true); // Habilita o LOG das ações executadas durante o envio do email  
 	return session;
     }  
 }  
   
-//clase que retorna uma autenticacao para ser enviada e verificada pelo servidor smtp  
+// Clase que retorna uma autenticacao para ser enviada e verificada pelo servidor smtp  
 class SimpleAuth extends Authenticator {  
     public String username = null;  
     public String password = null;  
