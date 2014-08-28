dfc20e03a005d4616eaddd4a7dd6f5cf55cf6ad5
Felipe Marques <felipesampaio93@hotmail.com>
2014-08-28 00:54:36 -0300

    identacao e comentarios classe EnderecoDAO

--- src/main/java/br/com/mdsgpp/guiaescolaideal/dao/EnderecoDAO.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/dao/EnderecoDAO.java
@@ -14,49 +14,51 @@ public class EnderecoDAO {
 
     private Connection connection;
 
-    public EnderecoDAO(Connection connection) {
+    public EnderecoDAO( Connection connection ) {
 	this.connection = connection;
     }
-
-    public Endereco pesquisarPorID(int id) throws SQLException {
+    
+    // Metodo para pesquisar determinado endereco por ID no banco
+    public Endereco pesquisarPorID( int id ) throws SQLException {
 	String sql = "select * from endereco where COD_ENDERECO = ?";
 
-	PreparedStatement stmt = this.connection.prepareStatement(sql);
-	stmt.setInt(1, id);
+	PreparedStatement stmt = this.connection.prepareStatement( sql );
+	stmt.setInt( 1, id );
 
 	ResultSet rs = stmt.executeQuery();
 	Endereco endereco = null;
 	
-	if (rs.next()) {
-	    endereco = getEndereco(rs);
+	if ( rs.next() ) {
+	    endereco = getEndereco( rs );
 	}
 
 	stmt.close();
 	return endereco;
     }
 
-    private Endereco getEndereco(ResultSet rs) throws SQLException {
+    // Metodo para pegar endereco de determinada escola
+    private Endereco getEndereco( ResultSet rs ) throws SQLException {
 	Endereco endereco = new Endereco();
 
-	endereco.setBairro(rs.getString("BAIRRO"));
-	endereco.setRua(rs.getString("RUA"));
-	endereco.setCep(Integer.parseInt(rs.getString("CEP")));
-	endereco.setNumero(rs.getString("NUMERO"));
-	endereco.setComplemento(rs.getString("COMPLEMENTO"));
+	endereco.setBairro(rs.getString( "BAIRRO" ));
+	endereco.setRua( rs.getString( "RUA" ) );
+	endereco.setCep( Integer.parseInt( rs.getString( "CEP" ) ) );
+	endereco.setNumero( rs.getString( "NUMERO" ) );
+	endereco.setComplemento( rs.getString( "COMPLEMENTO" ) );
 
 	Posicao posicao = new Posicao();
 
-	String latitude = rs.getString("LATITUDE");
-	String longitude = rs.getString("LONGITUDE");
+	String latitude = rs.getString( "LATITUDE" );
+	String longitude = rs.getString( "LONGITUDE" );
 
 	posicao.validaPosicao(latitude, longitude);
 	endereco.setPosicao(posicao);
 
 	Municipio municipio = null;
-	MunicipioDAO municipioDAO = new MunicipioDAO(connection);
-	municipio = municipioDAO.pesquisarPorId(ConversorDeEntrada
-		.getNumeroInteiroSemPonto(rs.getString("COD_MUNICIPIO")));
-	endereco.setMunicipio(municipio);
+	MunicipioDAO municipioDAO = new MunicipioDAO( connection );
+	municipio = municipioDAO.pesquisarPorId( ConversorDeEntrada
+		.getNumeroInteiroSemPonto( rs.getString( "COD_MUNICIPIO" ) ) );
+	endereco.setMunicipio( municipio );
 
 	return endereco;
     }
