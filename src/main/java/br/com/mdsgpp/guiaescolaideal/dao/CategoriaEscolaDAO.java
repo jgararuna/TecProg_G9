e4f877506800d4f3403f8ab9c74a201d8e16ab21
Felipe Marques <felipesampaio93@hotmail.com>
2014-08-28 00:48:20 -0300

    identacao e comentarios classe CategoriaEscolaDAO

--- src/main/java/br/com/mdsgpp/guiaescolaideal/dao/CategoriaEscolaDAO.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/dao/CategoriaEscolaDAO.java
@@ -10,29 +10,30 @@ import br.com.mdsgpp.guiaescolaideal.model.CategoriaEscola;
 public class CategoriaEscolaDAO {
 	private Connection connection;
 
-	public CategoriaEscolaDAO(Connection connection) {
+	public CategoriaEscolaDAO( Connection connection ) {
 		this.connection = connection;
 	}
 
-	public CategoriaEscola pesquisarPorID(int id) throws SQLException {
+	// Metodo para pesquisar determinada escola por ID do banco
+	public CategoriaEscola pesquisarPorID( int id ) throws SQLException {
 		String sql = "select * from tipo_rede_escola where COD_REDE = ?";
 
-		PreparedStatement stmt = this.connection.prepareStatement(sql);
+		PreparedStatement stmt = this.connection.prepareStatement( sql );
 		stmt.setInt(1, id);
 
 		ResultSet rs = stmt.executeQuery();
 		CategoriaEscola categoriaEscola = null;
 
-		if (rs.next()) {
-			categoriaEscola = pegarCategoriaEscola(rs);
+		if ( rs.next() ) {
+			categoriaEscola = pegarCategoriaEscola( rs );
 		}
 
 		stmt.close();
 		return categoriaEscola;
 
 	}
-
-	private CategoriaEscola pegarCategoriaEscola(ResultSet rs)
+	// Metodo para pegar a categoria da escola
+	private CategoriaEscola pegarCategoriaEscola( ResultSet rs )
 			throws SQLException {
 		CategoriaEscola categoriaEscola = new CategoriaEscola();
 
